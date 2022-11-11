cc47acccba94845d85e4fd5c711fb8d28ba715e7
jroargit <jroar26.git@gmail.com>
2022-11-11 10:48:57 -0500

    fix identation and create recovery password view

--- react-shop/src/containers/Layout.jsx
+++ react-shop/src/containers/Layout.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Layout = ( { children } ) => {
     return(
-       <div className="Layout">
+        <div className="Layout">
             {children}
-       </div>
+        </div>
     )
 };
 
