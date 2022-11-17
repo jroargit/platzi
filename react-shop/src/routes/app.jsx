2197a7f7a61a5d4863db447f72ebf5fecd7bd7fe
jroargit <jroar26.git@gmail.com>
2022-11-17 15:54:01 -0500

    create header nav bar and fix not found error

--- react-shop/src/routes/app.jsx
+++ react-shop/src/routes/app.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import '../styles/global.scss';
 import Layout from '../containers/Layout';
 import Login from '../containers/login';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RecoveryPassword from '../containers/RecoveryPassword';
+import NotFound from '../pages/NotFound';
+import Home from '../pages/Home';
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Switch>
-                <Layout>
-                    <Route exact path='/' component={Home}/>
-                    <Route exact path='login' component={Login}/>
-                    <Route exact path='/recovery-password' component={RecoveryPassword}/>
-                    <Route component={NotFound}/>
-                </Layout>
-            </Switch> 
+            <Layout>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/login' element={<Login/>}/>
+                    <Route path='/recovery-password' element={<RecoveryPassword/>}/>
+                    <Route path='/*' element={<NotFound/>}/>
+                </Routes>
+            </Layout> 
         </BrowserRouter>
     );
 }
