21f2d9eed575c7157b562ac21211c82b39ac3f10
jroargit <jroar26.git@gmail.com>
2022-11-17 16:03:55 -0500

    change class to className

--- react-shop/src/components/Header.jsx
+++ react-shop/src/components/Header.jsx
@@ -4,10 +4,10 @@ import "../styles/header.scss"
 const Header = () => {
     return (
         <nav>
-            <img src="./icons/icon_menu.svg" alt="menu" class="menu"/>
+            <img src="./icons/icon_menu.svg" alt="menu" className="menu"/>
 
-            <div class="navbar-left">
-                <img src="./logos/logo_yard_sale.svg" alt="logo" class="logo"/>
+            <div className="navbar-left">
+                <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo"/>
 
                 <ul>
                     <li>
@@ -31,10 +31,10 @@ const Header = () => {
                 </ul>
             </div>
 
-            <div class="navbar-right">
+            <div className="navbar-right">
                 <ul>
-                    <li class="navbar-email">platzi@example.com</li>
-                    <li class="navbar-shopping-cart">
+                    <li className="navbar-email">platzi@example.com</li>
+                    <li className="navbar-shopping-cart">
                         <img src="./icons/icon_shopping_cart.svg" alt="shopping cart"/>
                         <div>2</div>
                     </li>
