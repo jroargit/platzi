00fba59fcf01d8ad74bb168d45ca42bb8e7f0d9f
jroargit <jroar26.git@gmail.com>
2022-11-18 10:34:32 -0500

    Add every page to the routes in app.jsx

--- react-shop/src/routes/app.jsx
+++ react-shop/src/routes/app.jsx
@@ -6,6 +6,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import RecoveryPassword from '../pages/RecoveryPassword';
 import NotFound from '../pages/NotFound';
 import Home from '../pages/Home';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import CheckOut from '../pages/CheckOut';
+import Orders from '../pages/Orders';
 
 const App = () => {
     return (
@@ -15,6 +21,12 @@ const App = () => {
                     <Route path='/' element={<Home/>}/>
                     <Route path='/login' element={<Login/>}/>
                     <Route path='/recovery-password' element={<RecoveryPassword/>}/>
+                    <Route path='/send-email' element={<SendEmail/>}/>
+                    <Route path='/recovery-password' element={<NewPassword/>}/>
+                    <Route path='/recovery-password' element={<MyAccount/>}/>
+                    <Route path='/recovery-password' element={<CreateAccount/>}/>
+                    <Route path='/recovery-password' element={<CheckOut/>}/>
+                    <Route path='/recovery-password' element={<Orders/>}/>
                     <Route path='/*' element={<NotFound/>}/>
                 </Routes>
             </Layout> 
