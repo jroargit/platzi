7657430fc6cc7ee561fb83282bd868ad9938a796
jroargit <jroar26.git@gmail.com>
2022-11-18 15:05:26 -0500

    myOrder.jsx and scss code

--- react-shop/src/containers/MyOrder.jsx
+++ react-shop/src/containers/MyOrder.jsx
@@ -3,7 +3,49 @@ import "../styles/myOrder.scss"
 
 const MyOrder = () => {
     return (
-        <></>
+        <div className="my-order">
+            <div className="my-order-container">
+                <h1 className="title">My orders</h1>
+
+                <div className="my-order-content">
+                    <div className="order">
+                        <p>
+                            <span>03.25.21</span>
+                            <span>6 articles</span>
+                        </p>
+                        <p>$560.00</p>
+                        <img src="./icons/flechita.svg" alt="arrow"/>
+                    </div>
+
+                    <div className="order">
+                        <p>
+                            <span>03.25.21</span>
+                            <span>6 articles</span>
+                        </p>
+                        <p>$560.00</p>
+                        <img src="./icons/flechita.svg" alt="arrow"/>
+                    </div>
+
+                    <div className="order">
+                        <p>
+                            <span>03.25.21</span>
+                            <span>6 articles</span>
+                        </p>
+                        <p>$560.00</p>
+                        <img src="./icons/flechita.svg" alt="arrow"/>
+                    </div>
+
+                    <div className="order">
+                        <p>
+                            <span>03.25.21</span>
+                            <span>6 articles</span>
+                        </p>
+                        <p>$560.00</p>
+                        <img src="./icons/flechita.svg" alt="arrow"/>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
