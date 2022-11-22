856d3649ac4d8d25e79919bb92af528354d0c04f
jroargit <jroar26.git@gmail.com>
2022-11-22 10:39:34 -0500

    add @use vars in all scss fils

---
+++ react-shop/src/components/ProductItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'Styles/productItem.scss';
+import addToCart from 'Icons/bt_add_to_cart.svg';
+
+const productItemImg = "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+
+const ProductItem = () => {
+
+	return (
+		<div className="ProductItem">
+			<img src={productItemImg} alt="" />
+			<div className="product-info">
+				<div>
+					<p>$120,00</p>
+					<p>Bike</p>
+				</div>
+				<figure>
+					<img src={addToCart} alt="" />
+				</figure>
+			</div>
+		</div>
+	);
+}
+
+export default ProductItem;
