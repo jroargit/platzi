40e6c8e2b71421c1121547a517369199fb3a2b77
jroargit <jroar26.git@gmail.com>
2022-11-23 09:17:44 -0500

    now worths the list of items in the order jeje

--- react-shop/src/containers/MyOrder.jsx
+++ react-shop/src/containers/MyOrder.jsx
@@ -1,58 +1,33 @@
-import React, {useContext} from "react";
-import "../styles/myOrder.scss"
-import OrderItem from "../components/OrderItems";
-import AppContext from "../context/AppContext";
+import React, { useContext } from 'react';
+import OrderItem from 'Components/OrderItems';
+import AppContext from '../context/AppContext';
+import 'Styles/MyOrder.scss';
+import arrow from 'Icons/flechita.svg';
 
 const MyOrder = () => {
-    const {state} = useContext(AppContext);
-    return (
-        <div className="my-order">
-            <div className="my-order-container">
-                <h1 className="title">My orders</h1>
-
-                <div className="my-order-content">
-                    {state.cart.map(item => (
-                        <OrderItem product = {product} key={`orderItem-${product.id}`}/>
-                    ))}
-                    <div className="order">
-                        <p>
-                            <span>03.25.21</span>
-                            <span>6 articles</span>
-                        </p>
-                        <p>$560.00</p>
-                        <img src="./icons/flechita.svg" alt="arrow"/>
-                    </div>
-
-                    <div className="order">
-                        <p>
-                            <span>03.25.21</span>
-                            <span>6 articles</span>
-                        </p>
-                        <p>$560.00</p>
-                        <img src="./icons/flechita.svg" alt="arrow"/>
-                    </div>
-
-                    <div className="order">
-                        <p>
-                            <span>03.25.21</span>
-                            <span>6 articles</span>
-                        </p>
-                        <p>$560.00</p>
-                        <img src="./icons/flechita.svg" alt="arrow"/>
-                    </div>
-
-                    <div className="order">
-                        <p>
-                            <span>03.25.21</span>
-                            <span>6 articles</span>
-                        </p>
-                        <p>$560.00</p>
-                        <img src="./icons/flechita.svg" alt="arrow"/>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
+	const { state } = useContext(AppContext);
+	return (
+		<aside className="MyOrder">
+			<div className="title-container">
+				<img src={arrow} alt="arrow" />
+				<p className="title">My order</p>
+			</div>
+			<div className="my-order-content">
+				{state.cart.map(product => (
+					<OrderItem product={product} key={`orderItem-${product.id}`} />
+				))}
+				<div className="order">
+					<p>
+						<span>Total</span>
+					</p>
+					<p>$560.00</p>
+				</div>
+				<button className="primary-button">
+					Checkout
+				</button>
+			</div>
+		</aside>
+	);
+}
 
 export default MyOrder;
