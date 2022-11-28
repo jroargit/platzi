832f70de34c9c45507f8b96d1621fcbac6c36344
jroargit <jroar26.git@gmail.com>
2022-11-28 16:57:07 -0500

    add configuration of next.config.js

--- Next_Practical_course/store/next.config.js
+++ Next_Practical_course/store/next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  env: {
+    customKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: 'https://gndx.dev',
+        permanent: true,
+      }
+    ];
+  }
+};
