06aaef6ad60dc6c4929398fd5e507d98b8f5ffe3
jroargit <jroar26.git@gmail.com>
2022-12-07 08:42:44 -0500

    create console file to test diferent console objects and functions

---
+++ NodeJSIntroduccion/console.js
@@ -0,0 +1,16 @@
+const info = 45;
+const Object = {
+    "name": 'Juan Roa',
+    'age': 23,
+    'id': 1075320593, 
+}
+
+// Print info
+console.log();
+console.info();
+
+// Print an error
+console.error(Object);
+
+// Print info in a table
+console.table(Object);
