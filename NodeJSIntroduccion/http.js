a160d7b437752b0b5929de8861db752042349ce4
jroargit <jroar26.git@gmail.com>
2022-12-07 17:56:15 -0500

    http

--- NodeJSIntroduccion/http.js
+++ NodeJSIntroduccion/http.js
@@ -1,1 +1,11 @@
-const HTTP = require('http')
+const HTTP = require('http');
+const PORT = 3000;
+
+HTTP.createServer( function ( request, response) {
+    console.log("New request");
+    console.log(request.url);
+
+    response.write("HI! I'm using node.js http")
+
+    return response.end();
+}).listen(PORT);
